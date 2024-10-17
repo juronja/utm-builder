7c09df8c252acb942bb5cb70e836fb04d89e0736
juronja <101410098+juronja@users.noreply.github.com>
2024-10-17 21:49:52 +0200

    initial backed logic

---
+++ server.js
@@ -0,0 +1,78 @@
+import express from 'express'
+import { MongoClient, MongoServerError } from 'mongodb'
+
+// Definitions
+const MONGO_ADMIN_USER = 'see-notepad'
+const MONGO_ADMIN_PASS = 'see-notepad'
+const ISODate = new Date().toISOString()
+
+//const mongoEndpoint = new MongoClient(`mongodb://${MONGO_ADMIN_USER}:${MONGO_ADMIN_PASS}@localhost:27017`)
+const mongo = new MongoClient(`mongodb://localhost:27017`)
+const db = mongo.db('utm-builder')
+
+// Start express server
+const app = express()
+app.listen(3000, () => { console.log('Server is listening on port 3000') }) // define server listening port
+
+// Middleware
+app.use(express.json()) // needed to parse the JSON to JS first, otherwise you gat an error!
+//app.use(express.static('dist')) // serves the index.html file on load from the dist folder, so you can use the frontend app on the express app port (e.g. - localhost:3000). This is actually not needed if you configure the vite.config server.proxy!
+
+
+// GET method
+app.get('/users/:clientId/get-tagged-urls', async (req, res) => {
+    const clientId = req.params.clientId
+    
+    // Connect to DB
+    await mongo.connect()
+    console.log('Connected successfully to database')
+
+    // get data from db
+    const getUrls = await db.collection('taggedUrls')
+        .find({ clientId: clientId })
+        .sort({_id: -1}) // Sort by _id in descending order to get the latest items first
+        .project({ taggedUrl: 1, _id: 0 })
+        .toArray()
+    // const filteredUrls = getUrls.map(getUrls => getUrls.taggedUrl)
+    console.log('Got this URLs: ', getUrls)
+    
+    // Send a response to frontend
+    res.json(getUrls)
+    
+    // Disconnect
+    await mongo.close()
+    console.log('Disconnected successfully')
+
+})
+
+
+// POST method endpoint + RequestHandler function to handle requests and responses (req, res).
+app.post('/users/:clientId/save-tagged-url', async (req, res) => {
+    const payload = req.body
+    const clientId = req.params.clientId
+    
+    // Connect to DB
+    await mongo.connect()
+    console.log('Connected successfully to database')
+
+    // Save payload to DB
+    try {
+        payload['created'] = ISODate
+        await db.collection('taggedUrls').insertOne(payload)
+        console.log('Saved to DB: ', payload)
+
+    } catch(err) {
+        if (err instanceof MongoServerError) {
+            console.error(`Error worth logging: ${err}`)
+        }
+        throw err
+    }
+    
+    // Send a response to frontend
+    res.json(payload.taggedUrl)
+    
+    // Disconnect
+    await mongo.close()
+    console.log('Disconnected successfully')
+
+})
