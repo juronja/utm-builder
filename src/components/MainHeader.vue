ff2445968d1b54c5c0bab229dd2b500549ca8fc7
juronja <101410098+juronja@users.noreply.github.com>
2024-10-17 21:45:41 +0200

    removed color heading and logo

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-    <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="50" height="50" />
+    <!-- <img alt="UTM builder logo" class="logo" src="@/assets/logo.svg" width="50" height="50" /> -->
     <h1>{{ h1_text }}</h1>
 </template>
 
@@ -17,9 +17,4 @@ defineProps({
 .logo {
     margin-right: 1rem;
 }
-
-h1 {
-    color: var(--color-heading);
-}
-
 </style>
