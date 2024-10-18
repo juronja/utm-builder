58f11f6284c3df1356aea29a4d1e8de18beb1859
juronja <101410098+juronja@users.noreply.github.com>
2024-10-18 13:25:43 +0200

    added mongo to compose

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ let mongo = undefined
 if (ENV_LOCAL) { // Local environment
     mongo = new MongoClient(`mongodb://127.0.0.1:27017`)
 } else { // Docker compose dev
-    //mongo = new MongoClient(`mongodb://${MONGO_ADMIN_USER}:${MONGO_ADMIN_PASS}@127.0.0.1:27017`)
+    mongo = new MongoClient(`mongodb://${MONGO_ADMIN_USER}:${MONGO_ADMIN_PASS}@mongo`)
 }
 const db = mongo.db('utm-builder')
 
