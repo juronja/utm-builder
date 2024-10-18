84f798dcd919cf5dd0e91a0657e34e2d723b26bf
juronja <101410098+juronja@users.noreply.github.com>
2024-10-18 12:13:20 +0200

    compose commands rename

--- compose-dev-commands.sh
+++ compose-dev-commands.sh
@@ -4,6 +4,6 @@
 wget -O compose.yaml https://raw.githubusercontent.com/juronja/utm-builder/refs/heads/dev/compose.yaml
 
 # Stoping and starting the container
-docker-compose down
+docker compose down
 #docker image prune --force
-docker-compose up -d
+docker compose up -d
