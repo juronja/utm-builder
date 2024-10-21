19efa9812b3b1416f5bacd76f3d6b1466238fa64
juronja <101410098+juronja@users.noreply.github.com>
2024-10-21 12:15:15 +0200

    added viteExpress

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { MongoClient, MongoServerError } from 'mongodb'
+import ViteExpress from "vite-express";
 
 // Definitions
 const ISODate = new Date().toISOString()
@@ -7,7 +8,7 @@ const { MONGO_ADMIN_USER, MONGO_ADMIN_PASS, ENV_LOCAL } = process.env // Import 
 // add LINUX variables by editing "nano ~/.profile" and adding "export MONGO_ADMIN_USER=username"
 // add WINDOWS variables with "setx ENV_LOCAL true"
 // OR in docker compose environment
-
+// Start express server
 
 // Database endpoint check and connect
 let mongo = undefined
@@ -18,17 +19,15 @@ if (ENV_LOCAL) { // Local environment
 }
 const db = mongo.db('utm-builder')
 
+const app = express() // Initialize the express server
 
-// Start express server
-const app = express()
-app.listen(3000, () => { console.log('Server is listening on port 3000') }) // define server listening port
 
 // Middleware
 app.use(express.json()) // needed to parse the JSON to JS first, otherwise you gat an error!
 //app.use(express.static('dist')) // serves the index.html file on load from the dist folder, so you can use the frontend app on the express app port (e.g. - localhost:3000). This is actually not needed if you configure the vite.config server.proxy!
 
 // GET method
-app.get('/users/:clientId/get-tagged-urls', async (req, res) => {
+app.get('/api/users/:clientId/get-tagged-urls', async (req, res) => {
     const clientId = req.params.clientId
     
     // Connect to DB
@@ -55,7 +54,7 @@ app.get('/users/:clientId/get-tagged-urls', async (req, res) => {
 
 
 // POST method endpoint + RequestHandler function to handle requests and responses (req, res).
-app.post('/users/:clientId/save-tagged-url', async (req, res) => {
+app.post('/api/users/:clientId/save-tagged-url', async (req, res) => {
     const payload = req.body
     const clientId = req.params.clientId
     
@@ -84,3 +83,10 @@ app.post('/users/:clientId/save-tagged-url', async (req, res) => {
     console.log('Disconnected successfully')
 
 })
+
+
+
+// app.listen(3000, () => { console.log('Server is listening on port 3000') }) // replaced by ViteExpress
+ViteExpress.listen(app, 3000, () =>
+    console.log("Server is listening on http://localhost:3000"),
+  );
