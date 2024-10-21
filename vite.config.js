4348a878356025fa0c430eade215a72b2fad9878
juronja <101410098+juronja@users.noreply.github.com>
2024-10-21 12:14:11 +0200

    commented proxy

--- vite.config.js
+++ vite.config.js
@@ -13,14 +13,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: { // The idea is that you redirect all request that point to /api to express server root folder. Hence the rewrite. https://vite.dev/config/server-options.html#server-proxy
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Replace with your Express server's URL
-        changeOrigin: true,
-        //secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  // server: { // The idea is that you redirect all request that point to /api to express server root folder. Hence the rewrite. https://vite.dev/config/server-options.html#server-proxy
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000', // Replace with your Express server's URL
+  //       changeOrigin: true,
+  //       //secure: false,
+  //     }
+  //   }
+  // }
 })
