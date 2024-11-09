c559b3562c1762dfa24cfe0124460cbc3f95f672
juronja <101410098+juronja@users.noreply.github.com>
2024-11-09 08:38:36 +0100

    cleaned out comments

--- src/stores/url-recent.js
+++ src/stores/url-recent.js
@@ -2,24 +2,17 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useURLRecentStore = defineStore('url-recent', () => {
-  const data = ref('')
+  const data = ref([])
   let clientId = localStorage.getItem('clientId')
 
   async function fetchTaggedUrls() {
     try {
-      // const fetch = 'lul'
       const response = await fetch(`api/users/${clientId}/get-tagged-urls`)
       data.value = await response.json()
     } catch(err) {
       console.log(err)
     }
   }
-
-  // const doubleCount = computed(() => count.value * 2)
-
-  // function increment() {
-  //   count.value++
-  // }
 
   return { data, fetchTaggedUrls }
 })
