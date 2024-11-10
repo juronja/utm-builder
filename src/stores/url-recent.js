1959eb16bff39755da3865fdf3263ba2e36cefa3
juronja <101410098+juronja@users.noreply.github.com>
2024-11-10 09:53:31 +0100

    added a copy function

--- src/stores/url-recent.js
+++ src/stores/url-recent.js
@@ -5,6 +5,7 @@ export const useURLRecentStore = defineStore('url-recent', () => {
   const data = ref([])
   let clientId = localStorage.getItem('clientId')
 
+  // Fetch recent urls
   async function fetchTaggedUrls() {
     try {
       const response = await fetch(`api/users/${clientId}/get-tagged-urls`)
@@ -14,5 +15,18 @@ export const useURLRecentStore = defineStore('url-recent', () => {
     }
   }
 
-  return { data, fetchTaggedUrls }
+  // Copy li content
+  async function toClipboard(item, index) {
+    try {
+      await navigator.clipboard.writeText(item)
+      data.value[index].isCopied = true
+      setTimeout(() => { data.value[index].isCopied = false }, 1000)
+      console.log('Text copied:', item)
+    } catch(err) {
+      console.log('Cannot copy', err)
+    }
+  }
+
+
+  return { data, fetchTaggedUrls, toClipboard }
 })
