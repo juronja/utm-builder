7b0118c29dbefb640abdf980d81a938324547f39
juronja <101410098+juronja@users.noreply.github.com>
2024-11-17 14:36:42 +0100

    major code overhaul

--- src/stores/definitions.js
+++ src/stores/definitions.js
@@ -2,19 +2,72 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useDefinitionsStore = defineStore('utm-definitions', () => {
-  const mediumOptions = ref([
-  { data: "cpc" },
-  { data: "display" },
-  { data: "banner" },
-  { data: "content-text" },
-  { data: "email" },
-  { data: "affiliate" },
-  { data: "referral" },
-  { data: "direct" },
-  { data: "social" }
-])
+  let clientId = localStorage.getItem('clientId')
+  const isSaved = ref(false)
+  const isLoading = ref(false)
 
+  // All definitions
+  const data = ref([{
+    mediumDefinitions : ["affiliate", "cpc", "display", "banner", "content-text", "email", "referral", "direct", "social"]
 
+  }])
 
-  return { mediumOptions }
-})
+  // Add medium option to list (pinia)
+  function addMediumDefinition(newItem) {
+    const normalizedItem = newItem
+      .replace(/\s+/g, '-')
+      .replace(/š/g, 's')
+      .replace(/ž/g, 'z')
+      .replace(/č/g, 'c')
+      .replace(/ć/g, 'c')
+      .replace(/_/g, '-')
+      .replace(/\//g, '-')
+      .replace(/\\/g, '-')
+      .replace(/\./g, '-')
+      .toLowerCase()
+    data.value[0].mediumDefinitions.push( normalizedItem )
+  }
+
+  // Get All Definitions
+  async function getDefinitions() {
+    isLoading.value = true
+    try {
+      const response = await fetch(`api/users/${clientId}/get-definitions`)
+      data.value = await response.json()
+    } catch(err) {
+      console.error(err)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  // Save All Definitions
+  async function saveDefinitions() {
+    // Button text behavior
+    isSaved.value = true
+    setTimeout(() => { isSaved.value = false }, 1000)
+
+    try {
+    // Save for backend
+    const payload = {
+      mediumDefinitions: data.value[0].mediumDefinitions,
+      clientId: clientId
+    }
+
+    await fetch(`api/users/${clientId}/save-definitions`, {
+      method: 'POST', // Specify the HTTP method (POST in this case)
+      body: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    // return await response.json() // not sure if I need this return
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+    return { data, isSaved, isLoading, saveDefinitions, getDefinitions, addMediumDefinition }
+  })
+
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,19 +1,47 @@
 <script setup>
-import UtmRecent from '@/components/UtmRecent.vue';
+import { onBeforeMount, watchEffect } from 'vue'
+import { useDefinitionsStore } from '@/stores/definitions'
+
+const definitions = useDefinitionsStore()
+
+// watchEffect(() => {
+//   if (definitions.getDefinitions()) {
+//     onBeforeMount( () => {
+//       definitions.getDefinitions()
+//       console.log(definitions.data)
+//     })
+//   }
+// })
+
 </script>
 
 <template>
   <h2>Settings</h2>
-  <h3>Definitions</h3>
-    <div class="column">
-      <label for="add-medium">Medium options</label>
-      <input type="text" name="Add Medium Option" id="add-medium">
-      <ol>
-        <li>cpc</li>
-        <li>display</li>
-        <li>banner</li>
-      </ol>
+  <h3>Medium options</h3>
+  <div class="column section-box">
+    <label for="add-medium">Add item</label>
+    <div class="input-row">
+      <input @keyup.enter="definitions.addMediumDefinition($event.target.value); $event.target.value = ''" type="text" name="Add Medium Option" id="add-medium">
+      <span class="enter"><i class="bi bi-arrow-return-left"></i></span>
     </div>
+    <hr>
+    <div class="definitions-row">
+      <div v-if="definitions.isLoading">
+        Loading definitions ...
+      </div>
+      <div v-else>
+        <ul>
+          <li v-for="item in definitions.data[0].mediumDefinitions.sort()" :key="item">
+            {{ item }}
+            <span class="tag-del">x</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="save-btn section-gap">
+    <button @click="definitions.saveDefinitions">{{ definitions.isSaved ? 'Saved!' : 'Save' }}</button>
+  </div>
 
 </template>
 
@@ -22,11 +50,105 @@ import UtmRecent from '@/components/UtmRecent.vue';
 .column {
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
   flex: 0 0 auto;
-  width: 20%;
+  width: 100%;
 
 }
 
+.input-row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0.25rem 0 0 0;
+  min-height: 2.1rem;
+  color: var(--color-input-text);
+  background-color: var(--color-input-background);
+  border: 1px solid var(--color-border);
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  transition: 0.15s ease-in-out;
+}
 
+.definitions-row {
+  padding: 0;
+  font-size: 0.75rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: flex-start;
+  /* min-height: 3rem; */
+  /* max-height: 6rem; */
+  /* overflow-y: auto; adds scrollbar */
+  padding: 0.25rem;
+  background-color: var(--color-input-background);
+  border: 1px solid var(--color-border);
+  border-radius: 0.25rem;
+}
+
+input, select {
+  padding: var(--input-padding);
+  color: var(--color-input-text);
+  background-color: var(--color-input-background);
+  border: 0;
+}
+
+input:focus {
+  color: var(--color-input-text);
+  outline: 0;
+  border-color: var(--color-border-hover);
+  /* box-shadow: 0 0 0 .25rem var(--color-shadow-hover); */
+  border-radius: 0.25rem;
+}
+
+.enter {
+  padding: 0rem 0.4rem;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  font-size: 0.75rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: flex-start;
+  /* min-height: 3rem; */
+  /* max-height: 6rem; */
+  /* overflow-y: auto; adds scrollbar */
+  padding: 0.25rem;
+  background-color: var(--color-input-background);
+  border: 1px solid var(--color-border);
+  border-radius: 0.25rem;
+}
+
+ul li {
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  padding: var(--tag-padding);
+  margin: 2.5px;
+  color: var(--color-button-text);
+  border: 1px solid var(--color-border);
+  border-radius: 0.25rem;
+
+}
+
+.tag-del {
+  padding: 0.075rem 0.3rem;
+  margin-left: 0.4rem;
+  background-color: var(--color-danger);
+  border-radius: 0.25rem;
+}
+
+.save-btn {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
 
 </style>
