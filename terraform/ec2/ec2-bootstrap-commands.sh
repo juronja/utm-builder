6188e5274db0815d1f29fe4cb77ffbb2fcd7cc0f
juronja <101410098+juronja@users.noreply.github.com>
2025-06-19 08:37:55 +0200

    commands in new lines

--- terraform/ec2/ec2-bootstrap-commands.sh
+++ terraform/ec2/ec2-bootstrap-commands.sh
@@ -8,4 +8,6 @@ sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 
 # Install docker compose
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m) -o /usr/bin/docker-compose && sudo chmod 755 /usr/bin/docker-compose && docker-compose --version
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m) -o /usr/bin/docker-compose
+sudo chmod 755 /usr/bin/docker-compose
+docker-compose --version
